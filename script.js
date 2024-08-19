b12f8ccf0327dda53ea2ec041d2d4b3539751eac
Thomas Gurr <tpgurr21@gmail.com>
2024-08-18 22:00:01 -0400

    installed open source with npm

--- script.js
+++ script.js
@@ -86,3 +86,22 @@ console.log(ShoppingCart2.shippingCost)
 // const { addToCart } = require('./shoppingCart.js');
 
 */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js'
+
+const state = {
+    cart: [
+        {product: 'bread', quantity: 5},
+        {product: 'pizza', quantity: 5},
+    ],
+    user: {
+        loggedIn: true
+    },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
